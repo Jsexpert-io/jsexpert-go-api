cfe632da9138b93bcf73fc447fca976357a257f1
bhavanad05 <bhavana.nd05@gmail.com>
2024-03-31 12:13:49 +0530

    Initial changeS

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	traceController "golangproject/controller"
 	"golangproject/database"
+	"golangproject/middelwear"
 
 	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
@@ -18,7 +19,7 @@ func main() {
   traceRouteGroup := router.Group("/v1/traces") 
   {
 
-	//traceRouteGroup.Use(middelwear.ProjectAuth())
+	traceRouteGroup.Use(middelwear.ProjectAuth())
 	traceRouteGroup.GET("", traceController.GetTraces)
 	traceRouteGroup.GET("/:id", traceController.GetTraceById)
 	traceRouteGroup.POST("", traceController.MapTrace)
